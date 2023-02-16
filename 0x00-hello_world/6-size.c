385de03b93d76000169c33dbfa4389fcad18835d
Ligala Noah <ligalanoah@gmail.com>
2023-02-16 13:50:07 -0800

    Size of Granduer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("\nSize of an int: %lu byte(s)", sizeof(int));
 	printf("\nSize of a long int: %lu byte(s)", sizeof(long));
 	printf("\nSize of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("\nSize of a float: %lu byte(s)\n", sizeof(float));
+	printf("\nSize of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
