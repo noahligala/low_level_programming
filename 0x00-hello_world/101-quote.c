75345166d42ae515860b8243c4daff2934256230
Ligala Noah <ligalanoah@gmail.com>
2023-02-16 14:56:37 -0800

    Standard Error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"" +
+		       " - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
