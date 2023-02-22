21da5fdd373e8df86adf24d481dbbfc279d793d5
Ligala Noah <ligalanoah@gmail.com>
2023-02-21 20:47:20 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-	for (j = 0 ; j < 10 ; j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
 			k = j * i;
 			if (k > 9)
@@ -24,6 +24,7 @@ void times_table(void)
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			if (j != 9)
