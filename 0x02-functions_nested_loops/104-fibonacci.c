54a4b19b9456e751e3468b370187617dcfa520cb
Ligala Noah <ligalanoah@gmail.com>
2023-02-21 19:17:19 -0800

    98 fibonacci terms

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long int n1 = 1;
+	unsigned long int n2 = 2;
+	unsigned long int n3;
+	unsigned long int count;
+
+	printf("1, 2, ");
+	for (count = 3 ; count <= 98 ; count++)
+	{
+		n3 = n1 + n2;
+		printf("%lu", n3);
+		n1 = n2;
+		n2 = n3;
+	if (count != 98)
+	{
+		printf(",");
+		printf(" ");
+	}
+	else
+		printf("\n");
+	}
+	return (0);
+}
