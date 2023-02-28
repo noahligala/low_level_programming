00db092206a4c6191075825cd5aea589516b65f9
Ligala Noah <ligalanoah@gmail.com>
2023-02-28 12:04:02 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *strcpy - function to copy string 
+ *
+ * @dest: function parameter
+ *
+ * @src: function parameter
+ *
+ * Return: returns value of dest when succesful
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
