2e40ff3ff738e49b91b3a756499c337dc890cc33
Ligala Noah <ligalanoah@gmail.com>
2023-02-27 16:41:54 -0800

    puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - function to print to stdout
+ *
+ * @str; function parameter
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
