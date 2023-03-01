c4d0d3f37b1f4e34c19ccf629ab02a523f6c926d
Ligala Noah <ligalanoah@gmail.com>
2023-03-01 06:11:59 -0800

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * strcat - function to append the src string to dest
+ * @src: function parameter
+ * @dest: function parameter
+ * Return: dest on success
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+	{
+		len++;
+	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
