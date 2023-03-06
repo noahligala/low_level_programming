43aea46c34f4e4f6c195439bf56f39a47836b607
Ligala Noah <ligalanoah@gmail.com>
2023-03-06 15:12:43 -0800

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function to print the chessboard
+ * @a: array
+ * Return Always 0 (Succss)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\n');
+	}
+}
