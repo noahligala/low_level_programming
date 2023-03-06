e43370cd9e4bf437a434cda9242386eaff9cafbe
Ligala Noah <ligalanoah@gmail.com>
2023-03-06 14:43:25 -0800

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - function to get the length if a prefix substring
+ * @s: function parameter
+ * @accept: function parameter
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *p = s;
+	char *a;
+	while (*p) 
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*p == *a)
+			{
+				count++;
+				break;
+			}
+			a++;
+		}
+		if (*a == '\0')
+		{
+			/*character not found in accept*/
+			return (count);
+		}
+		p++;
+	}
+	return (count);
+}
