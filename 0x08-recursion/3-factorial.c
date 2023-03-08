89b019866c31cc862a76e6b184bb7a32d6ea26fb
Ligala Noah <ligalanoah@gmail.com>
2023-03-08 11:28:17 -0800

    3-Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that calculates the factorial
+ * of a given number
+ * @n: integer for which the factorial is calculated
+ * Return: integer value
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
