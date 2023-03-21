97d5d09a3a862aa3beeccc3944e377daf522ac8f
Ligala Noah <ligalanoah@gmail.com>
2023-03-21 15:17:28 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog Attributes
+ * @name: The name of the dog
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ *
+ * Description: The attributed of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for Dog structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
