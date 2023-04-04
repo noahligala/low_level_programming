eff394d3513a5a82e17f2af554dda63be2aac80d
Ligala Noah <ligalanoah@gmail.com>
2023-04-04 03:56:31 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		count++;
 		h = h->next;
-		node_count++;
 	}
-	return (node_count);
+
+	return (count);
 }
