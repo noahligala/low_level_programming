a91c78882e74c4034d1adbfe20b6e3d0244b94c9
noahligala <63068195+noahligala@users.noreply.github.com>
2023-09-07 15:48:02 +0000

    '2-calloc.c'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,49 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
-* *string_nconcat - function that concatenates two strings and returns
-* pointer to the longer string.
-* @s1: first string / destination string
-* @s2: second string to be concatenated to the first string.
-* 
-* Return: Pointer to the concatenated string
-*/
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-    
+ * string_nconcat - Concatenates two strings up to n bytes from s2.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes from s2 to concatenate.
+ *
+ * Return: A pointer to the newly allocated concatenated string, or NULL on failure.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n) {
+    unsigned int len1 = 0, len2 = 0;
+    unsigned int i, j;
+    char *result;
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    // Calculate the length of s1 and s2
+    while (s1[len1] != '\0')
+        len1++;
+    while (s2[len2] != '\0')
+        len2++;
+
+    // Adjust n if it's greater than or equal to the length of s2
+    if (n >= len2)
+        n = len2;
+
+    // Allocate memory for the result string
+    result = (char *)malloc((len1 + n + 1) * sizeof(char));
+    if (result == NULL)
+        return NULL;
+
+    // Copy s1 to the result string
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
+
+    // Concatenate up to n bytes of s2 to the result string
+    for (j = 0; j < n; j++)
+        result[i + j] = s2[j];
+
+    // Add the null terminator
+    result[i + j] = '\0';
+
+    return result;
 }
